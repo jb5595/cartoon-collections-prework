3e79ffc57fdece3ed4c2771724ee014c419f58b0
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 21:39:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index} #{dwarf} \n"
+  end 
+
 end
 
 def summon_captain_planet# code an argument here
