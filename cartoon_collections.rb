43c6d92e4acd95e29eb307daf76ade12b5b36591
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 21:43:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ end
 def summon_captain_planet(calls) # code an argument here
   # Your code here
   calls.collect do |call|
-    calls.capit
+    call.capitalize
+    call+="!"
 end
 
 def long_planeteer_calls# code an argument here
